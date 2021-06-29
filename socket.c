594ba872e36672ccacf41a5e246b0518e5a41a8e
Abijuru  Seth <abiheloaf@gmail.com>
2021-06-29 19:11:49 +0200

    Fix: removed dependency errors

--- socket.c
+++ socket.c
@@ -25,70 +25,71 @@ int BindCreatedSocket(int hSocket) {
 
 int main(int argc , char *argv[])
 {
-        int socket_desc , sock , clientLen , read_size;
-        struct sockaddr_in server , client;
-        char client_message[200]={0};
-        char message[100] = {0};
-        const char *pMessage = "hello
-        aticleworld.com"; //Create socket
-        socket_desc = SocketCreate();
-        if (socket_desc == -1)
+    int socket_desc , sock , clientLen , read_size;
+    struct sockaddr_in server , client;
+    char client_message[200]={0};
+    char message[100] = {0};
+    const char *pMessage = "hello
+    aticleworld.com"; //Create socket
+    socket_desc = SocketCreate();
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+        return 1;
+    }
+    printf("Socket created\n");
+    //Bind
+    if( BindCreatedSocket(socket_desc) < 0)
+    {
+        //print the error message
+        perror("bind failed.");
+        return 1;
+    }
+    printf("bind done\n");
+    //Listen
+    listen(socket_desc , 3);
+    //Accept and incoming connection
+    while(1)
+    {
+        printf("Waiting for incoming connections...\n");
+        clientLen = sizeof(struct sockaddr_in);
+        //accept connection from an incoming client
+        sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&clientLen);
+        if (sock < 0)
         {
-            printf("Could not create socket");
+            perror("accept failed");
             return 1;
         }
-        printf("Socket created\n");
-        //Bind
-        if( BindCreatedSocket(socket_desc) < 0)
+        printf("Connection accepted\n");
+        memset(client_message, '\0', sizeof
+        client_message); memset(message, '\0', sizeof
+        message); //Receive a reply from the client
+        if( recv(sock , client_message , 200 , 0) < 0)
         {
-            //print the error message
-            perror("bind failed.");
+            printf("recv failed");
+            break;
+        }
+
+        printf("Client reply : %s\n",client_message);
+        if(strcmp(pMessage,client_message)==0)
+        {
+            strcpy(message,"Hi there !");
+        }
+        else
+        {
+            strcpy(message,"Invalid Message !");
+        }
+
+        // Send some data
+        if( send(sock , message , strlen(message) , 0) < 0)
+        {
+            printf("Send failed");
             return 1;
         }
-        printf("bind done\n");
-        //Listen
-        listen(socket_desc , 3);
-        //Accept and incoming connection
-        while(1)
-        {
-            printf("Waiting for incoming connections...\n");
-            clientLen = sizeof(struct sockaddr_in);
-            //accept connection from an incoming client
-            sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&clientLen);
-            if (sock < 0)
-            {
-                perror("accept failed");
-                return 1;
-            }
-            printf("Connection accepted\n");
-            memset(client_message, '\0', sizeof
-            client_message); memset(message, '\0', sizeof
-            message); //Receive a reply from the client
-            if( recv(sock , client_message , 200 , 0) < 0)
-            {
-                printf("recv failed");
-                break;
-            }
-
-            printf("Client reply : %s\n",client_message);
-            if(strcmp(pMessage,client_message)==0)
-            {
-                strcpy(message,"Hi there !");
-            }
-            else
-            {
-                strcpy(message,"Invalid Message !");
-            }
-
-            // Send some data
-            if( send(sock , message , strlen(message) , 0) < 0)
-            {
-                printf("Send failed");
-                return 1;
-            }
-            
-            close(sock);
-            sleep(1);
-        }
+        
+        close(sock);
+        sleep(1);
+    }
+    
     return 0;
 }
